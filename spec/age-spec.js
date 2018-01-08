7a83d4f40c9d52dad682a6aa14215d43eeb92bbe
Guest User <Guest@Epicodus-3E.local>
2018-01-08 08:11:27 -0800

    got the first test to pass

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,8 +1,10 @@
+import { Age } from './../js/scripts.js';
+
 
 describe('Age', function() {
 
   it('should test whether output is a number', function() {
-    var barf = new Age(2)
-    expect(barf.toSecs()).toBeNumber();
+    var barf = new Age()
+    expect(barf.toSecs(2)).toBeNumber();
   });
 });
